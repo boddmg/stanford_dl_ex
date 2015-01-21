f4bd5b930534f99c82e6b2142a98213036ef99f0
boddmg <boddmg@gmail.com>
2015-01-22 01:07:43 +0800

    Finish the ex1
    Signed-off-by: boddmg <boddmg@gmail.com>

--- ex1/linear_regression.m
+++ ex1/linear_regression.m
@@ -1,24 +1,40 @@
 function [f,g] = linear_regression(theta, X,y)
-  %
-  % Arguments:
-  %   theta - A vector containing the parameter values to optimize.
-  %   X - The examples stored in a matrix.
-  %       X(i,j) is the i'th coordinate of the j'th example.
-  %   y - The target value for each example.  y(j) is the target for example j.
-  %
-  
-  m=size(X,2);
-  n=size(X,1);
+    %
+    % Arguments:
+    %   theta - A vector containing the parameter values to optimize.
+    %   X - The examples stored in a matrix.
+    %       X(i,j) is the i'th coordinate of the j'th example.
+    %   y - The target value for each example.  y(j) is the target for example j.
+    %
 
-  f=0;
-  g=zeros(size(theta));
+    m=size(X,2);
+    n=size(X,1);
 
-  %
-  % TODO:  Compute the linear regression objective by looping over the examples in X.
-  %        Store the objective function value in 'f'.
-  %
-  % TODO:  Compute the gradient of the objective with respect to theta by looping over
-  %        the examples in X and adding up the gradient for each example.  Store the
-  %        computed gradient in 'g'.
+    f=0;
+    g=zeros(size(theta));
+
+    %
+    % TODO:  Compute the linear regression objective by looping over the examples in X.
+    %        Store the objective function value in 'f'.
+    %
+    % TODO:  Compute the gradient of the objective with respect to theta by looping over
+    %        the examples in X and adding up the gradient for each example.  Store the
+    %        computed gradient in 'g'.
+
+    %%% YOUR CODE HERE %%%
+    %m,n,size(theta,1)
+    for i=1:m
+        f = f + (theta' * X(:,i) - y(i))^2;
+    end
+    f = f/2;
+    for j=1:n
+        sum = 0;
+        for i=1:m
+           sum  = sum + X(j,i) * (theta' * X(:,i) - y(i));
+        end
+        g(j) = g(j) + sum;
+    end
+    
+    
+
   
-%%% YOUR CODE HERE %%%
